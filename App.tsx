1383cd7187674e4d85e2e0fc357ac3bec9b858df
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-11-18 20:53:13 -0300

    Inicializando projeto

--- App.tsx
+++ App.tsx
@@ -10,11 +10,20 @@ export default function App() {
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    alignItems: 'center',
+  }
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
